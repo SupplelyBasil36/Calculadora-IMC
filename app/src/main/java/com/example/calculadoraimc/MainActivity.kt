f74f2260ee2abbecadb9e65dfc72e23137bbedd9
Luis Montoya <skyfire5990@gmail.com>
2026-04-09 20:46:58 -0600

    nothing

--- app/src/main/java/com/example/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.calculadoraimc
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,13 +12,19 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-            //comentario
+
+        val edtPesoKg = findViewById<EditText>(R.id.edtPesoKG)
+        val edtAlturaM = findViewById<EditText>(R.id.edtAlturaM)
+        val calcular = findViewById<Button>(R.id.btnCalcular)
+        val resultado = findViewById<TextView>(R.id.txtResultado)
+        var imc: Double
+
+        calcular.setOnClickListener {
+            val pesoKg = edtPesoKg.text.toString().toDouble()
+            val alturaM = edtAlturaM.text.toString().toDouble()
+            imc = pesoKg / (alturaM * alturaM)
+            resultado.text = "Tu IMC es: ${"%.2f".format(imc)}"
         }
     }
 }
